0bfe9adcc61ddbe2fdef0fb6942616bd65f584a3
zakrow <rakzowland@gmail.com>
2023-10-01 15:43:27 -0600

    Heads up display

--- scripts/main.gd
+++ scripts/main.gd
@@ -6,7 +6,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -15,10 +15,13 @@ func _process(delta):
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready!")
 	$StartTimer.start()
 
 func _on_start_timer_timeout():
@@ -27,6 +30,7 @@ func _on_start_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_mob_timer_timeout():
 	var mob = mob_scene.instantiate()
